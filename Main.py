ee7bf4099a20bc29ec428bd303dc078692360807
mormasa <51478618+mormasa@users.noreply.github.com>
2019-12-15 23:20:15 +0200

    Get my asins from file Get page asins Look my asins in page asins

--- Main.py
+++ Main.py
@@ -1,29 +1,52 @@
 from selenium import webdriver
 import time
 
+
+def get_page_asins():
+    all_products = chrome.find_elements_by_xpath('.//div[@class = "s-result-list s-search-results sg-row"]//div[@data-asin]')
+    asins = []
+    for item in all_products:
+        asins.append(item.get_attribute("data-asin"))
+    return asins
+
+
+def get_my_asins_from_inventory_txt_file(inventory_filename):
+    parsed_data = []
+    with open(inventory_filename) as file:
+        data = [line.strip() for line in file.readlines()]
+    for line in data:
+        parsed_data.append(line.split("\t"))
+    my_asins = [parsed_data[i][10] for i in range(1, len(parsed_data))]
+    my_asins = list(dict.fromkeys(my_asins))    # Removing duplications
+    return my_asins
+
+
+def look_my_product_in_page(my_asins, page_asins):
+    for asin in my_asins:
+        if asin in page_asins:
+            print(f"ASIN {asin} is in page {i+1}, position: {page_asins.index(asin)}")
+
+
+def go_to_next_page():
+    next_button = chrome.find_elements_by_xpath('.//li[@class = "a-last"]')
+    next_button[0].click()
+    time.sleep(5)
+
+
+inventory_filename = "inventory.txt"
 amazon_url = "http://www.amazon.com"
 results = 0
 string_to_check = "Chihuahua Coffee Mug"
-my_product = f"Funny {string_to_check}"
-print(my_product)
+my_asins = get_my_asins_from_inventory_txt_file(inventory_filename)
+
 chrome = webdriver.Chrome()
 chrome.get(amazon_url)
 chrome.find_element_by_id("twotabsearchtextbox").send_keys(string_to_check)
 chrome.find_element_by_id("twotabsearchtextbox").submit()
 for i in range(10):
     print(f"Checking page number {i+1}")
-    all_products = chrome.find_elements_by_xpath('.//span[@class = "a-size-base-plus a-color-base a-text-normal"]')
-    num_of_all_products = len(all_products)
-    # print(f" Number of results for page {i+1}: {num_of_all_products}")
-    for product in all_products:
-        product_name = product.text
-        if my_product in product_name:
-            print(f"product was found on page {i+1}")
-    # if results:
-    #     break
-    # print(f"{my_product} wasn't found on page {i+1}. Moving to page {i+2}")
-    next_button = chrome.find_elements_by_xpath('.//li[@class = "a-last"]')
-    next_button[0].click()
-    time.sleep(5)
+    page_asins = get_page_asins()
+    look_my_product_in_page(my_asins, page_asins)
+    go_to_next_page()
 print("Done searching. Closing browser..")
 chrome.quit()
