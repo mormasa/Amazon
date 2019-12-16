fee6f00b156fb627910fd37ff4a997163473adca
mormasa <51478618+mormasa@users.noreply.github.com>
2019-12-16 21:33:18 +0200

    multiple keywords

--- Main.py
+++ Main.py
@@ -16,37 +16,55 @@ def get_my_asins_from_inventory_txt_file(inventory_filename):
         data = [line.strip() for line in file.readlines()]
     for line in data:
         parsed_data.append(line.split("\t"))
-    my_asins = [parsed_data[i][10] for i in range(1, len(parsed_data))]
-    my_asins = list(dict.fromkeys(my_asins))    # Removing duplications
+    my_asins = [[parsed_data[i][10], parsed_data[i][2]] for i in range(1, len(parsed_data))]
+
+# Remove duplications
+    for asin in my_asins:
+        orig = True
+        for j in range(0, len(my_asins)):
+            if asin[0] == my_asins[j][0]:
+                if not orig:
+                    my_asins.remove(asin)
+                    break
+                orig = False
     return my_asins
 
 
-def look_my_product_in_page(my_asins, page_asins):
+def look_my_product_in_page(my_asins, page_asins, page_number):
     for asin in my_asins:
-        if asin in page_asins:
-            print(f"ASIN {asin} is in page {i+1}, position: {page_asins.index(asin)}")
+        if asin[0] in page_asins:
+            print(f"{asin[1]} ASIN {asin[0]} is in page {page_number}, position: {page_asins.index(asin[0])}")
 
 
 def go_to_next_page():
     next_button = chrome.find_elements_by_xpath('.//li[@class = "a-last"]')
     next_button[0].click()
-    time.sleep(5)
+    time.sleep(2)
 
+try:
+    inventory_filename = "inventory.txt"
+    amazon_url = "http://www.amazon.com"
+    results = 0
+    string_to_check = ["Yorkie Coffee Mug", "Chihuahua Coffee Mug", "Labrador Coffee Mug", "Dachshund Coffee Mug",
+                       "German Shepherd Coffee Mug", "Border Collie Coffee Mug", "Pitbull Coffee Mug", "Pit Bull Coffee Mug"]
+    my_asins = get_my_asins_from_inventory_txt_file(inventory_filename)
 
-inventory_filename = "inventory.txt"
-amazon_url = "http://www.amazon.com"
-results = 0
-string_to_check = "Chihuahua Coffee Mug"
-my_asins = get_my_asins_from_inventory_txt_file(inventory_filename)
-
-chrome = webdriver.Chrome()
-chrome.get(amazon_url)
-chrome.find_element_by_id("twotabsearchtextbox").send_keys(string_to_check)
-chrome.find_element_by_id("twotabsearchtextbox").submit()
-for i in range(10):
-    print(f"Checking page number {i+1}")
-    page_asins = get_page_asins()
-    look_my_product_in_page(my_asins, page_asins)
-    go_to_next_page()
-print("Done searching. Closing browser..")
-chrome.quit()
+    chrome = webdriver.Chrome()
+    chrome.get(amazon_url)
+    for keyword in string_to_check:
+        search_bar = chrome.find_element_by_id("twotabsearchtextbox")
+        search_bar.send_keys(keyword)
+        search_bar.submit()
+        print(f"---------------- printing ranking for keyword: {keyword}")
+        for i in range(3):
+            print(f"Checking page number {i+1}")
+            page_asins = get_page_asins()
+            look_my_product_in_page(my_asins, page_asins, i+1)
+            go_to_next_page()
+        chrome.find_element_by_id("twotabsearchtextbox").clear()
+    print("Done searching. Closing browser..")
+    chrome.quit()
+except IndexError as e:
+    print("Last page. Done")
+    print(str(e))
+    chrome.quit()
