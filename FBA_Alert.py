82cf878ed689fff2c2d5b776bd5ad859e6284292
mormasa <51478618+mormasa@users.noreply.github.com>
2020-07-10 13:47:24 +0300

    FBA_Alert tool

--- FBA_Alert.py
+++ FBA_Alert.py
@@ -3,49 +3,65 @@ import pandas as pd
 sales_file = "Sales.csv"
 inventory_file = "FBA_Inventory.csv"
 report_file = "FBA_Inventory_Alert_Report.xlsx"
-
+skus_to_ignore = ['1001']
 
 def get_info_from_csv(filename):
-    with open(filename) as f:
-        file_out = [line.strip().split(',') for line in f.readlines()]
+
     updated_output = []
-    for i in range(1, len(file_out)):
+
+    # if filename == sales_file:
+    with open(filename) as f:
+        file_out = [line.strip().split('"') for line in f.readlines()]
+    for j in range(len(file_out)):
+        if j ==0:
+            continue
         new_line = []
-        if filename == sales_file:
-            new_line.append([file_out[i][1], '', file_out[i][7], int(file_out[i][7]) * 2, '', ''])
-        elif filename == inventory_file:
-            try:
-                if int(file_out[i][0]) < 1000 or int(file_out[i][0]) > 1100:
-                    continue
-            except Exception as e:
-                print(f"Exception {str(e)}")
-                if "1013A" in file_out[i][0] or "GB-4288389-20-White_FBA" in file_out[i][0]:
-                    new_line.append([file_out[i][2], file_out[i][0], '', '', file_out[i][9], ''])
-                continue
-            new_line.append([file_out[i][2], file_out[i][0], '', '', file_out[i][9], ''])
-        updated_output.append(new_line[0])
+        line_to_check = file_out[j]
+        for i in range(len(line_to_check)):
+            if not (',' in line_to_check[i] and len(line_to_check[i]) == 1):
+                new_line.append(line_to_check[i])
+        updated_output.append(new_line)
     return updated_output
 
 
-def update_fba_info_with_sales(fba_info, sales_info):
+def update_fba_info_with_sales(fba_info, sales_info, skus_to_ignore):
+    report = [['ASIN', 'SKU', 'TITLE', '14 Days Sales', '28 Days Sales', 'Inventory', 'Incoming', 'Inventory/28 Days Sales']]
+
     for i in range(len(fba_info)):
+        product_sku = fba_info[i][1]
+        if product_sku in skus_to_ignore:
+            continue
+        product_name = fba_info[i][4]
+        fba_report_asin = fba_info[i][3]
+        inventory_qty = int(fba_info[i][10])
+        incoming = int(fba_info[i][17])
+
         for j in range(len(sales_info)):
-            if fba_info[i][0] == sales_info[j][0]:
-                fba_info[i][2] = sales_info[j][2]
-                fba_info[i][3] = sales_info[j][3]
-    return fba_info
+            updated = False
+            sales_report_asin = sales_info[j][2]
 
-
-def update_alert_ratio(report):
-    for i in range(len(report)):
-        if not report[i][3]:
-            report[i][2] = 0
-            report[i][3] = 0
-        if int(report[i][3]) == 0:
-            report[i][5] = 999999
+            if fba_report_asin == sales_report_asin:
+                last_14_days_sales = int(sales_info[j][9])
+                last_28_days_sales = last_14_days_sales * 2
+                updated = True
+                break
+        if not updated:
+            last_14_days_sales = 0
+            last_28_days_sales = 0
+            if inventory_qty > 0:
+                ratio = 999999
+            else:
+                ratio = 0
         else:
-            report[i][5] = float(int(report[i][4])/report[i][3])
+            if last_14_days_sales == 0:
+                ratio = 999999
+            elif inventory_qty > 0:
+                ratio = round(float(inventory_qty / last_28_days_sales), 2)
+            else:
+                ratio = 0
+        report.append([fba_report_asin, product_sku, product_name, last_14_days_sales, last_28_days_sales, inventory_qty, incoming, ratio])
     return report
+
 
 
 def build_report(data):
@@ -76,22 +92,30 @@ def build_report_panda(data):
     asin = []
     sku = []
     sales14 = []
-    sales30 = []
+    sales28 = []
     inventory = []
+    incoming = []
     ratio = []
-    for line in data:
-        asin.append(line[0])
-        sku.append(line[1])
-        sales14.append(line[2])
-        sales30.append(line[3])
-        inventory.append(line[4])
-        ratio.append(line[5])
+    title = []
+    for i in range(len(data)):
+        if i == 0:
+            continue
+        asin.append(data[i][0])
+        title.append(data[i][2])
+        sku.append(data[i][1])
+        sales14.append(data[i][3])
+        sales28.append(data[i][4])
+        inventory.append(data[i][5])
+        incoming.append(data[i][6])
+        ratio.append(data[i][7])
     writer = pd.ExcelWriter(report_file, engine='xlsxwriter')
     df = pd.DataFrame({'Asin': asin,
+                       'Title': title,
                        'SKU': sku,
                        'Sales 14 days': sales14,
-                       'Sales 30 days': sales30,
+                       'Sales 30 days': sales28,
                        'Inventory': inventory,
+                       'Incoming': incoming,
                        'Inventory/Monthly Sales': ratio})
     df = df.sort_values('Inventory/Monthly Sales')
     print(df)
@@ -102,12 +126,12 @@ def build_report_panda(data):
     yellow = workbook.add_format({'bg_color': '#F7FE2E'})
 
 
-    worksheet1.conditional_format(f'G2:G{len(data)}', {'type': 'cell',
+    worksheet1.conditional_format(f'I2:I{len(data)}', {'type': 'cell',
                                       'criteria': 'between',
                                       'minimum': 0.01,
                                       'maximum': 0.5,
                                       'format': red})
-    worksheet1.conditional_format(f'G2:G{len(data)}', {'type': 'cell',
+    worksheet1.conditional_format(f'I2:I{len(data)}', {'type': 'cell',
                                       'criteria': 'between',
                                       'minimum': 0.501,
                                       'maximum': 1,
@@ -118,10 +142,7 @@ def build_report_panda(data):
 
 sales_info = get_info_from_csv(sales_file)
 fba_info = get_info_from_csv(inventory_file)
-report = update_fba_info_with_sales(fba_info, sales_info)
-report = update_alert_ratio(report)
-# report.insert(0, ["ASIN", "SKU", "Sales 14 days", "Prediction for 30 days", "Current Inventory", "Inventory/Monthly Sales"])
+report = update_fba_info_with_sales(fba_info, sales_info, skus_to_ignore)
 build_report_panda(report)
 # print(report)
-# build_report(report)
 print("Done!")
